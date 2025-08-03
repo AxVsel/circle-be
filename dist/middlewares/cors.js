e1d42664fee3cbf48c740df00d9ac3acdbb60197
Muh. Yaumil Aksah Hamid <assperacus77@gmail.com>
2025-08-03 15:17:40 +0800

    cors add frontend

--- dist/middlewares/cors.js
+++ dist/middlewares/cors.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const corsMiddleware = (0, cors_1.default)({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://circle-fe-a3mm.vercel.app"],
     credentials: true,
 });
 exports.default = corsMiddleware;
--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://circle-fe-a3mm.vercel.app"],
   credentials: true,
 });
 
